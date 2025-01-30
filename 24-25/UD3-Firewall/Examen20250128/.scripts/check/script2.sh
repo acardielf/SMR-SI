16da15df3fc99df47b4d474f9b938a0efde02b37
Ángel Cardiel Ferrero <angel.cardiel@protonmail.com>
2025-01-30 01:42:08 +0100

    improve timeouts in check scrips

--- 24-25/UD3-Firewall/Examen20250128/.scripts/check/script2.sh
+++ 24-25/UD3-Firewall/Examen20250128/.scripts/check/script2.sh
@@ -5,7 +5,7 @@ echo "Criterio 2: El servidor web puede conectarse al servidor de base de datos 
 # Verificar conexión desde web a db. 
 # Netcat con -z indica que no se envía datos, solo se verifica la conexión.
 # con -v es verbose
-connection=$(vagrant ssh web -c "nc -vz 192.168.56.102 3306" 2>&1)
+connection=$(vagrant ssh web -c "nc -vz -w 5 192.168.56.102 3306" 2>&1)
 if [[ $connection == *"succeeded"* ]]; then
     echo "[✔] Web can connect to DB on port 3306."
 else
