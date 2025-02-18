bf4e776b64275f77d9e3362f56b0a3f8b9fcf352
Ángel Cardiel Ferrero <acarfer940@g.educaand.es>
2025-02-18 11:04:40 +0100

    installing wp

--- 24-25/UD4-Backup/Ejercicio202502/.scripts/initial_vm1.sh
+++ 24-25/UD4-Backup/Ejercicio202502/.scripts/initial_vm1.sh
@@ -51,5 +51,26 @@ ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 
 
 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get install -y php8.2 php-cli php-common php-mbstring php-xml php-curl php-zip php-gd php-intl php-bcmath php-mysql php-sqlite3 unzip composer
+sudo apt-get install -y apache2 mariadb-server unzip php8.4 php-cli php-common php-mbstring php-xml php-curl php-zip php-gd php-intl php-bcmath php-mysql php-sqlite3 unzip composer
 
+WP_OWNER=www-data
+WP_GROUP=www-data
+WP_ROOT=/var/www/html
+WS_GROUP=www-data 
+
+curl --output-dir /tmp/ -O https://wordpress.org/latest.zip 
+unzip /tmp/latest.zip -d ${WP_ROOT}
+mv -i ${WP_ROOT}/wordpress/{.,}* ${WP_ROOT}/
+rmdir ${WP_ROOT}/wordpress
+rm /tmp/latest.zip
+
+sudo find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
+sudo find ${WP_ROOT} -type d -exec chmod 755 {} \;
+sudo find ${WP_ROOT} -type f -exec chmod 644 {} \;
+
+sudo chgrp ${WS_GROUP} ${WP_ROOT}/wp-config.php
+sudo chmod 660 ${WP_ROOT}/wp-config.php
+
+sudo find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
+sudo find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
+sudo find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
