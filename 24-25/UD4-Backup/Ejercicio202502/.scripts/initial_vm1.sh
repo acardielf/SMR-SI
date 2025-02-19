069107fc9f64200f2c5b58eab33b4b450c3d02be
Ángel Cardiel Ferrero <angel.cardiel@protonmail.com>
2025-02-19 23:46:30 +0100

    improve wp

--- 24-25/UD4-Backup/Ejercicio202502/.scripts/initial_vm1.sh
+++ 24-25/UD4-Backup/Ejercicio202502/.scripts/initial_vm1.sh
@@ -50,7 +50,6 @@ sudo iptables -X
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 
 
-sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get install -y apache2 mariadb-server php php-cli php-common php-mbstring php-xml php-curl php-zip php-gd php-intl php-bcmath php-mysql unzip composer
 
 
@@ -64,20 +63,28 @@ DB_PASSWORD='salmedina_pass'
 DB_HOST='localhost'
 DB_CHARSET='utf8mb4'
 
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS '$DB_NAME' CHARACTER SET $DB_CHARSET;"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET $DB_CHARSET;"
 sudo mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASSWORD';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'$DB_HOST';"
 sudo mysql -e "FLUSH PRIVILEGES;"
 
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -s -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 php /usr/local/bin/wp --info
 
 sudo rm -rf ${WP_ROOT}/*
 
-sudo -u www-data wp core download --path="${WP_ROOT}"
-sudo -u www-data wp config create --path="${WP_ROOT}" --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --locale=es_ES
-sudo -u www-data wp core install --path="${WP_ROOT}" --url=192.168.56.103 --title="Salmedina SMR" --admiwp adin_user=salmedina --admin_password=salmedina --admin_email=admin@salmedina.com
+sudo chown -R www-data:www-data ${WP_ROOT}
 
+echo "Downloading Wordpress..."
+sudo -u www-data wp core download --path="${WP_ROOT}"
+
+echo "Creating wp-config.php..."
+sudo -u www-data wp config create --path="${WP_ROOT}" --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --locale=es_ES
+
+echo "Installing Wordpress..."
+sudo -u www-data wp core install --path="${WP_ROOT}" --url=192.168.56.103 --title="Salmedina SMR" --admin_user=salmedina --admin_password=salmedina --admin_email=admin@salmedina.com
+
+echo "Installing Wordpress language..."
+sudo -u www-data wp language core install es_ES --path="${WP_ROOT}"
